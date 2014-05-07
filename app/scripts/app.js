3188b19e1fba80b6bbcaf2fdbed766e41bef1dbb
ldproj <ldproj@users.noreply.github.com>
2014-05-07 17:26:30 +0100

    Add angular routing, activate boilerplate pages
    includes update to css

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,28 @@
 'use strict';
 
-angular.module('masterStumblefuzzApp', ['ngResource', 'ngCookies']);
+angular.module('masterStumblefuzzApp', ['ngResource', 'ngRoute', 'ngCookies'])
+.config(function($routeProvider, $locationProvider){
+	
+	$locationProvider.html5Mode(true);
+
+	$routeProvider
+	.when('/', {
+		templateUrl: 'partials/main',
+		controller: 'MainCtrl'
+	})
+	.when('/login', {
+		templateUrl: 'partials/login',
+		controller: 'LoginCtrl'
+	})
+	.when('/settings', {
+		templateUrl: 'partials/settings',
+		controller: 'SettingsCtrl'
+	})
+	.when('/signup', {
+		templateUrl: 'partials/signup',
+		controller: 'SignupCtrl'
+	})
+	.otherwise({
+		rediretTo: '/'
+	});
+});
